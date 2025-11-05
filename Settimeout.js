dda7a419ff24597436ffe92e39e97e3bc7129286
swathi-17-sep <swathiberry1796@gmail.com>
2025-11-05 11:28:34 +0530

    Day_2_setTimeout_task

---
+++ Settimeout.js
@@ -0,0 +1,16 @@
+console.log("movie describe by using settimeout method");
+ 
+console.log("movie name:Irandam Ulagam");
+
+    setTimeout(()=>{
+        console.log("heroine fall in love with hero but hero refused");
+        setTimeout(()=>{
+            console.log("after few days ago hero accepts her love they went some trip at the time heroine met a accedent");
+            setTimeout(()=>{
+                console.log("after heroine dead hero some how enter in new universe where heroine is alive");
+                setTimeout(()=>{
+                    console.log("finally they both lived happily ever after");
+                },2000);
+            },2000);
+        },2000);
+    },3000);
